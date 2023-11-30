842c458948ac57e94d2de00db180fcba7650eccd
Karthick181998 <karthickbe43@gmail.com>
2023-11-30 17:25:40 +0530

    Add template css styles add

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
+  // multistyle css files-> styleUrls:['1','2']
+  styles: [`
+  h1{
+    color:blue;
+  }
+  `]
 })
 export class AppComponent {
 }
